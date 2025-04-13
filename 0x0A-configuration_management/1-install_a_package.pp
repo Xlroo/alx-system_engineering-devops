80a63e81496ef0e84f95d4c5f0c3edfb67a69a02
Xlroo <80978847+Xlroo@users.noreply.github.com>
2025-04-13 14:44:47 +0300

    Trial

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/bin/pup
-#Install an especific version of flask (2.1.0)
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  unless  => 'pip3 list | grep flask',
+  provider => 'pip3'
 }
