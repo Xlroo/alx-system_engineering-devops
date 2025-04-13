fcceeeacc0857dcf127641356592e675448ebe58
Xlroo <80978847+Xlroo@users.noreply.github.com>
2025-04-13 14:40:02 +0300

    Add unless

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,7 @@
-File Edit Options Buffers Tools Puppet Help
- #!/usr/bin/pup
- #Install an especific version of flask (2.1.0)
- package {'flask':
-   ensure   => '2.1.0',
-   provider => 'pip3',
-   unless  => 'pip3 list | grep flask',
-  }
+#!/usr/bin/pup
+#Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  unless  => 'pip3 list | grep flask',
+}
