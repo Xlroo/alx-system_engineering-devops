b15d2eeb7cf7e7c9a2a1ec41f6bc4faeed4d2f9a
Xlr00 <80978847+Xlroo@users.noreply.github.com>
2025-04-19 05:09:50 -0700

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
-file { '/home/ubuntu/.ssh/config':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  content => @("CONFIG")
-Host myserver
-  HostName 54.89.142.178
-  User ubuntu
-  IdentityFile /home/ubuntu/.ssh/school
-  PasswordAuthentication no
-| CONFIG,
-}
+#!/usr/bin/env bash
+#using puppet to make changes to my configuration file
 
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+  content => "
+  #SSH client configuration
+  Host*
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+  ",
+}
